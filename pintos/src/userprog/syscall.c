62e1880066edeb4a2f5adf93acf14053364fc8ca
ReveNEspoir <psu9808@naver.com>
2016-10-24 17:08:39 +0900

    proj2 - implement syscall

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -5,51 +5,88 @@
 #include "threads/thread.h"
 
 static void syscall_handler (struct intr_frame *);
+/*na-10.23 make syscall_handler, syscall_halt(), syscall_read(),syscall_write()*/
+
+//for #proj2_1 make functions//
+void syscall_halt(void);
+void syscall_exit (int status); 
+pid_t syscall_exec (const char *cmd_line); 
+int syscall_wait (pid_t pid); 
+int syscall_read (int fd, void *buffer, unsigned size);
+int syscall_write (int fd, const void *buffer, unsigned size);
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
-
+/*na-10.23*/
 static void
 syscall_handler (struct intr_frame *f UNUSED) //intr_frame : src/threads/interrupt.h
 {
+    int syscallnum;
   printf ("system call!\n");
+  
+  syscallnum = f->esp;
+
+  
+  if (syscallnum == SYS_HALT){
+    syscall_halt();
+  }
+  else if (syscallnum == SYS_EXIT){
+
+  }
+  else if (syscallnum == SYS_EXEC){
+
+  }
+  else if (syscallnum == SYS_WAIT){
+
+  }
+  else if (syscallnum== SYS_READ){
+
+  }else if (syscallnum== SYS_WRITE){
+
+  }
+
+
+
+
+ 
   thread_exit ();
 }
-/*
+
 void
-halt (void) 
+syscall_halt (void) 
 {
   shutdown_power_off();
 }
 
 void
-exit (int status) 
+syscall_exit (int status) 
 {
 
     return status;
 }
 
 pid_t
-exec (const char *cmd_line) 
+syscall_exec (const char *cmd_line) 
 {
 }
 
 int
-wait (pid_t pid) 
+syscall_wait (pid_t pid) 
 {
 }
 
 int
-read (int fd, void *buffer, unsigned size)
+syscall_read (int fd, void *buffer, unsigned size)
 {//we just make this part which enable stdin stdout to execute
+    
 }
 int
-write (int fd, const void *buffer, unsigned size)
+syscall_write (int fd, const void *buffer, unsigned size)
 {
-}*/
+}
 /********************************
  * this function must be prototyped.
  *
