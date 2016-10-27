fb951c33a853b594b8bfd5c9b908414071733cad
undergraduate <cse20141499@cspro.sogang.ac.kr>
2016-10-27 20:56:18 +0900

    add sum.c

---
+++ pintos/src/examples/sum.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "syscall.h"
+#incldue "../lib/user/syscall.h"
+
+int
+main(int argc, char **argv)
+{
+    int a,b,c,d;
+    int fib;
+    int sum = 0;
+
+    a = atoi(argv[1]);
+    b = atoi(argv[2]);
+    c = atoi(argv[3]);
+    d = atoi(argv[4]);
+
+    sum = sum_of_four_integers( a, b, c, d);
+    fib = syscall_fibonacci(a);
+    printf("%d %d\n",fib, sum);
+
+    return 0;
+}
